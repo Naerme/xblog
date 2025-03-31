be3d354ace1b91ca4d02ab9d19c0ad12dd66fba4
pity <1078037654@qq.com>
2025-03-31 16:08:14 +0800

    feat-评论树-163

--- testdata/24.评论递归.go
+++ testdata/24.评论递归.go
@@ -13,13 +13,33 @@ func main() {
 	global.Conifg = core.ReadConf()
 	core.InitLogrus()
 	global.DB = core.InitDB()
-	// 2 -> 8 -> 9
-	rootComment := GetRootComment(3)
-	fmt.Println(rootComment.ID)
-	rootComment = GetRootComment(2)
-	fmt.Println(rootComment.ID)
-	rootComment = GetRootComment(1)
-	fmt.Println(rootComment.ID)
+	//// 2 -> 8 -> 9
+	//rootComment := GetRootComment(3)
+	//fmt.Println(rootComment.ID)
+	//rootComment = GetRootComment(2)
+	//fmt.Println(rootComment.ID)
+	//rootComment = GetRootComment(1)
+	//fmt.Println(rootComment.ID)
+
+	//model := models.CommentModel{
+	//	Model: models.Model{ID: 2},
+	//}
+	//GetCommentTree(&model)
+
+	model := GetCommentTreeV3(1)
+	fmt.Println(model.ID)
+	for _, c1 := range model.SubCommentList {
+		fmt.Println("1:", c1.ID)
+		for _, c2 := range c1.SubCommentList {
+			fmt.Println("2:", c2.ID)
+			for _, c3 := range c2.SubCommentList {
+				fmt.Println("3:", c3.ID)
+				for _, c4 := range c3.SubCommentList {
+					fmt.Println("4:", c4.ID)
+				}
+			}
+		}
+	}
 }
 
 // GetRootComment 获取一个评论的根评论
@@ -37,3 +57,23 @@ func GetRootComment(commentID uint) (model *models.CommentModel) {
 }
 
 // 查一个评论下的子评论
+// 评论树
+func GetCommentTree(model *models.CommentModel) {
+	global.DB.Preload("SubCommentList").Take(model)
+	for _, commentModel := range model.SubCommentList {
+		GetCommentTree(commentModel)
+	}
+}
+
+func GetCommentTreeV3(id uint) (model *models.CommentModel) {
+	model = &models.CommentModel{
+		Model: models.Model{ID: id},
+	}
+	global.DB.Preload("SubCommentList").Take(model)
+	for i := 0; i < len(model.SubCommentList); i++ {
+		commentModel := model.SubCommentList[i]
+		item := GetCommentTreeV3(commentModel.ID)
+		model.SubCommentList[i] = item
+	}
+	return
+}
