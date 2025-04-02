9332da3ec8e07d99543bb78c1127fa01ccfa6344
pity <1078037654@qq.com>
2025-04-02 22:57:33 +0800

    feat-站内信系统消息-187

--- service/message_service/enter.go
+++ service/message_service/enter.go
@@ -99,3 +99,16 @@ func InsertCollectArticleMessage(model models.UserArticleCollectModel) {
 		logrus.Error(err)
 	}
 }
+func InsertSystemMessage(revUserID uint, title string, content string, linkTitle string, linkHref string) {
+	err := global.DB.Create(&models.MessageModel{
+		Type:      message_type_enum.SystemType,
+		RevUserID: revUserID,
+		Title:     title,
+		Content:   content,
+		LinkTitle: linkTitle,
+		LinkHref:  linkHref,
+	}).Error
+	if err != nil {
+		logrus.Error(err)
+	}
+}
