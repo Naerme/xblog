c85e2ec83229c982dcb65b30c2f258626504236d
pity <1078037654@qq.com>
2025-04-06 16:46:25 +0800

    feat-ws消息已读-bug-229

--- models/ctype/chat_msg/enter.go
+++ models/ctype/chat_msg/enter.go
@@ -11,8 +11,13 @@ type MarkdownMsg struct {
 	Content string `json:"content"`
 }
 
+type MsgReadMsg struct {
+	ReadChatID uint `json:"readChatID"` // 哪一条消息被读取了
+}
+
 type ChatMsg struct {
 	TextMsg     *TextMsg     `json:"textMsg,omitempty"`
 	ImageMsg    *ImageMsg    `json:"imageMsg,omitempty"`
 	MarkdownMsg *MarkdownMsg `json:"markdownMsg,omitempty"`
+	MsgReadMsg  *MsgReadMsg  `json:"msgReadMsg,omitempty"`
 }
