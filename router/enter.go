cfb898fa8a53ad55e7beee31ad96f2d2f2df0187
pity <1078037654@qq.com>
2025-04-09 15:40:27 +0800

    feat-前端发起sse请求-259

--- router/enter.go
+++ router/enter.go
@@ -13,7 +13,7 @@ func Run() {
 	r.Static("/uploads", "uploads")
 
 	nr := r.Group("/api")
-	nr.Use(middleware.LogMiddleware)
+	nr.Use(middleware.LogMiddleware).Use(middleware.Cors())
 	SiteRouter(nr)
 	LogRouter(nr)
 	ImageRouter(nr)
